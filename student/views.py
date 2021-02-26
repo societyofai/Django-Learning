3f00a8eb10080f8a4fa1b1dc2b1f492619b18433
Gurjas Singh <gurjassingh92@gmail.com>
2021-02-26 20:32:46 +0530

    filter details and display

--- student/views.py
+++ student/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Student
 
 def index(request):
-    return render(request, 'student/index.html')
+    # data = Student.objects.all() # querying the data
+    data = Student.objects.filter(studentClass='10') # querying the data
+    details = {
+        'data': data
+    }
+    return render(request, 'student/index.html', details)
 
